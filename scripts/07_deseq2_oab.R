6b497ddf38c1fcabbbea43cb9072eea281ec7631
kimbaraR <kimbara.ryosuke32@gmail.com>
2026-05-10 00:31:01 +0900

    Update and rename 07_deseq2.R to 07_deseq2_oab.R

---
+++ scripts/07_deseq2_oab.R
@@ -0,0 +1,117 @@
+llibrary(tximport)
+library(DESeq2)
+library(pheatmap)
+library(RColorBrewer)
+
+dir.create("../deseq2/oab", recursive=TRUE, showWarnings=FALSE)
+
+samples <- read.table(
+  "../metadata/sample_list_oab.txt",
+  stringsAsFactors=FALSE
+)$V1
+
+files <- file.path("../salmon", samples, "quant.sf")
+names(files) <- samples
+
+txi <- tximport(files, type="salmon", txOut=TRUE)
+
+tx2gene <- data.frame(
+  TXNAME = rownames(txi$counts),
+  GENEID = rownames(txi$counts)
+)
+
+gene_exp <- summarizeToGene(
+  txi,
+  tx2gene,
+  countsFromAbundance="scaledTPM"
+)
+
+count <- round(gene_exp$counts)
+
+coldata <- read.table(
+  "../metadata/conditions_oab.tsv",
+  header=TRUE,
+  row.names=1
+)
+
+dds <- DESeqDataSetFromMatrix(
+  countData=count,
+  colData=coldata,
+  design=~condition
+)
+
+dds <- dds[rowSums(counts(dds)) >= 10,]
+
+dds$condition <- relevel(dds$condition, ref="Ab")
+
+dds <- DESeq(dds)
+
+res <- results(dds)
+
+write.csv(
+  as.data.frame(res[order(res$padj),]),
+  "../deseq2/oab/DESeq2_O_vs_Ab.csv"
+)
+
+deg <- subset(as.data.frame(res), padj < 0.05)
+
+oral <- subset(deg, log2FoldChange > 0)
+aboral <- subset(deg, log2FoldChange < 0)
+
+write.table(
+  rownames(oral),
+  "../deseq2/oab/oral_genes.txt",
+  quote=FALSE,
+  row.names=FALSE,
+  col.names=FALSE
+)
+
+write.table(
+  rownames(aboral),
+  "../deseq2/oab/aboral_genes.txt",
+  quote=FALSE,
+  row.names=FALSE,
+  col.names=FALSE
+)
+
+
+############################
+# sample distance heatmap
+############################
+
+vsd <- vst(dds, blind = FALSE)
+
+sampleDists <- dist(t(assay(vsd)))
+
+sampleDistMatrix <- as.matrix(sampleDists)
+
+rownames(sampleDistMatrix) <- vsd$condition
+colnames(sampleDistMatrix) <- vsd$condition
+
+colors <- colorRampPalette(
+  rev(brewer.pal(9, "Blues"))
+)(255)
+
+png("oab_sample_distance_heatmap.png")
+
+pheatmap(
+  sampleDistMatrix,
+  clustering_distance_rows = sampleDists,
+  clustering_distance_cols = sampleDists,
+  col = colors
+)
+
+dev.off()
+
+############################
+# PCA plot
+############################
+
+png("oab_PCA.png")
+
+plotPCA(
+  vsd,
+  intgroup = c("condition")
+)
+
+dev.off()
