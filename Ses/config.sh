9867db403beaaa33593c92d254c3846c54cc2460
kimbaraR <kimbara.ryosuke32@gmail.com>
2026-05-09 20:09:49 +0900

    Update config.sh

--- Ses/config.sh
+++ Ses/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-THREADS=8
-SPECIES=Ses #Acanthosepion esculentum
+THREADS=12
+SPECIES=Ses # Acanthosepion esculentum
 
 BASE_DIR=~/data/Ses_bulk_RNA-seq_2nd-arm
 
@@ -10,20 +10,22 @@ TRIM_DIR=$BASE_DIR/trimmed
 FASTQC_DIR=$BASE_DIR/fastqc
 ASSEMBLY_DIR=$BASE_DIR/assembly
 SALMON_DIR=$BASE_DIR/salmon
-
+METADATA_DIR=Ses/metadata
 ADAPTER=$BASE_DIR/adapter_Ses.fa
 
-TRINITY_OUT=$ASSEMBLY_DIR/trinity
-
-# original Trinity assembly
-TRINITY_FASTA=$TRINITY_OUT/Trinity.fasta
-
-# cd-hit output
-CDHIT_FASTA=$ASSEMBLY_DIR/Trinity_cdhit.fa
+# Assembly outputs
+TRINITY_ABO=$ASSEMBLY_DIR/Trinity_Ses2nd_oab
+TRINITY_PMD=$ASSEMBLY_DIR/Trinity_Ses2nd_pmd
 
 # downstream transcriptome
+TRINITY_FASTA=$TRINITY_PMD/Trinity.fasta
+
+# cd-hit output
+CDHIT_FASTA=$ASSEMBLY_DIR/Trinity_Ses2nd_pmd_cdhit.fa
+
+# downstream reference
 TRANSCRIPTOME=$CDHIT_FASTA
 
 SALMON_INDEX=$SALMON_DIR/index
 
-METADATA_DIR=Ses/metadata
+
