34160e19ddd18d85f36540168a5733a5d0498267
kimbaraR <kimbara.ryosuke32@gmail.com>
2026-05-10 10:34:00 +0900

    Update 01_qc.sh

--- scripts/01_qc.sh
+++ scripts/01_qc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # usage:
-# bash 01_qc.sh ../Ses/config.sh
-# bash 01_qc.sh ../Afang/config.sh
-# bash 01_qc.sh ../Lst/config.sh
+# bash scripts/01_qc.sh Ses/config.sh
+# bash scripts/01_qc.sh Afang/config.sh
+# bash scripts/01_qc.sh Lst/config.sh
 
 source "$1"
 
