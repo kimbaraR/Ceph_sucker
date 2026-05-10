0ff72f5635afdc00f3cc668983561a52d3c7a393
kimbaraR <kimbara.ryosuke32@gmail.com>
2026-05-10 09:59:53 +0900

    Update 01_qc.sh

--- scripts/01_qc.sh
+++ scripts/01_qc.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-source ../config/config.sh
+# usage:
+# bash 01_qc.sh ../Ses/config.sh
+# bash 01_qc.sh ../Afang/config.sh
+# bash 01_qc.sh ../Lst/config.sh
+
+source "$1"
 
 mkdir -p $FASTQC_DIR
 
