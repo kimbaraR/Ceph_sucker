625cf298f8cd6da5964e68d84dbb8f853e581269
kimbaraR <kimbara.ryosuke32@gmail.com>
2026-05-10 14:29:59 +0900

    Update 09_extract_deg_fasta.sh

--- scripts/09_extract_deg_fasta.sh
+++ scripts/09_extract_deg_fasta.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-source $1
+# usage:
+# bash scripts/09_extract_deg_fasta.sh Ses/config.sh
 
-mkdir -p ../deg_analysis
-mkdir -p ../deg_analysis/fasta
+source "$1"
+
+mkdir -p Ses/deg_analysis
+mkdir -p Ses/deg_analysis/fasta
 
 ##################################################
 # positional DEG overlap (PMD)
@@ -10,54 +13,54 @@ mkdir -p ../deg_analysis/fasta
 
 # Distal > Middle AND Distal > Proximal
 grep -Fxf \
-../deseq2/pmd/distal_DM_genes.txt \
-../deseq2/pmd/distal_DP_genes.txt \
-> ../deg_analysis/distal_PMD_genes.txt
+Ses/deseq2/pmd/distal_DM_genes.txt \
+Ses/deseq2/pmd/distal_DP_genes.txt \
+> Ses/deg_analysis/distal_PMD_genes.txt
 
 # Middle > Distal AND Middle > Proximal
 grep -Fxf \
-../deseq2/pmd/middle_DM_genes.txt \
-../deseq2/pmd/middle_MP_genes.txt \
-> ../deg_analysis/middle_PMD_genes.txt
+Ses/deseq2/pmd/middle_DM_genes.txt \
+Ses/deseq2/pmd/middle_MP_genes.txt \
+> Ses/deg_analysis/middle_PMD_genes.txt
 
 # Proximal > Distal AND Proximal > Middle
 grep -Fxf \
-../deseq2/pmd/proximal_DP_genes.txt \
-../deseq2/pmd/proximal_MP_genes.txt \
-> ../deg_analysis/proximal_PMD_genes.txt
+Ses/deseq2/pmd/proximal_DP_genes.txt \
+Ses/deseq2/pmd/proximal_MP_genes.txt \
+> Ses/deg_analysis/proximal_PMD_genes.txt
 
 ##################################################
 # extract fasta
 ##################################################
 
 seqkit grep \
--f ../deg_analysis/distal_PMD_genes.txt \
+-f Ses/deg_analysis/distal_PMD_genes.txt \
 $TRANSCRIPTOME \
--o ../deg_analysis/fasta/Distal.fa
+-o Ses/deg_analysis/fasta/Distal.fa
 
 seqkit grep \
--f ../deg_analysis/middle_PMD_genes.txt \
+-f Ses/deg_analysis/middle_PMD_genes.txt \
 $TRANSCRIPTOME \
--o ../deg_analysis/fasta/Middle.fa
+-o Ses/deg_analysis/fasta/Middle.fa
 
 seqkit grep \
--f ../deg_analysis/proximal_PMD_genes.txt \
+-f Ses/deg_analysis/proximal_PMD_genes.txt \
 $TRANSCRIPTOME \
--o ../deg_analysis/fasta/Proximal.fa
+-o Ses/deg_analysis/fasta/Proximal.fa
 
 ##################################################
 # overlap between Distal and Oral
 ##################################################
 
-mkdir -p ../deg_analysis/overlap
+mkdir -p Ses/deg_analysis/overlap
 
 # distal & oral
 grep -Fxf \
-../deg_analysis/distal_PMD_genes.txt \
-../deseq2/oab/oral_genes.txt \
-> ../deg_analysis/overlap/distal_oral_genes.txt
+Ses/deg_analysis/distal_PMD_genes.txt \
+Ses/deseq2/oab/oral_genes.txt \
+> Ses/deg_analysis/overlap/distal_oral_genes.txt
 
 seqkit grep \
--f ../deg_analysis/overlap/distal_oral_genes.txt \
+-f Ses/deg_analysis/overlap/distal_oral_genes.txt \
 $TRANSCRIPTOME \
--o ../deg_analysis/overlap/distal_oral.fa
+-o Ses/deg_analysis/overlap/distal_oral.fa
