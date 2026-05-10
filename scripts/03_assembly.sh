c15d4cfa50bc7d98a9e506672d7187fc37c9ac47
kimbaraR <kimbara.ryosuke32@gmail.com>
2026-05-10 10:32:55 +0900

    Update 03_assembly.sh

--- scripts/03_assembly.sh
+++ scripts/03_assembly.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
-# usage:
-# bash 03_assembly.sh ../Ses/config.sh
-# bash 03_assembly.sh ../Afang/config.sh
-# bash 03_assembly.sh ../Lst/config.sh
 
 source "$1"
 
-mkdir -p $ASSEMBLY_DIR
+ASSEMBLY_LIST="$2"
+ASSEMBLY_OUTPUT="$3"
 
-LEFT=$(ls $TRIM_DIR/*_R1P.fq.gz | paste -sd ",")
-RIGHT=$(ls $TRIM_DIR/*_R2P.fq.gz | paste -sd ",")
+mkdir -p "$ASSEMBLY_DIR"
+
+LEFT=$(while read sample; do
+    echo -n "$TRIM_DIR/${sample}_R1P.fq.gz,"
+done < "$ASSEMBLY_LIST" | sed 's/,$//')
+
+RIGHT=$(while read sample; do
+    echo -n "$TRIM_DIR/${sample}_R2P.fq.gz,"
+done < "$ASSEMBLY_LIST" | sed 's/,$//')
 
 Trinity \
 --seqType fq \
---left $LEFT \
---right $RIGHT \
---CPU $THREADS \
---max_memory 50G \
---output $TRINITY_OUT
+--left "$LEFT" \
+--right "$RIGHT" \
+--CPU "$THREADS" \
+--max_memory 128G \
+--output "$ASSEMBLY_OUTPUT"
+
+# usage (Ses: oral-aboral assembly)
+# bash scripts/03_assembly.sh \
+# Ses/config.sh \
+# Ses/metadata/assembly_ab_o.txt \
+# ~/data/Ses_bulk_RNA-seq_2nd-arm/assembly/Trinity_Ses2nd_oab
+
+# usage (Afang)
+# bash scripts/03_assembly.sh \
+# Afang/config.sh \
+# Afang/metadata/sample_list.txt \
+# ~/Af_rnaseq/assembly/trinity_out
